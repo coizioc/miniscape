7d1713afc9be3b34121112a3490319d89f77d38a
coizioc <36041795+coizioc@users.noreply.github.com>
2018-08-19 17:19:09 +0200

    Add files via upload

--- mathbot.py
+++ mathbot.py
@@ -8,33 +8,45 @@ import config
 def extensions_generator():
     """Returns a generator for all cog files that aren't in do_not_use."""
     cog_path = "./cogs"
-    do_not_use = ["__init__.py", "__pycache__"]
+    do_not_use = ["__init__.py", "__pycache__", 'cap.py', 'pet.py', 'reddit.py', 'rs.py',
+                  'solver.py', 'stats.py', 'telos.py', "memers.py"]
     for cog in os.listdir(cog_path):
         if cog not in do_not_use:
             yield f"cogs.{cog[:-3]}"
+    # use = ["stats.py"]
+    # for cog in os.listdir(cog_path):
+    #     if cog in use:
+    #         yield f"cogs.{cog[:-3]}"
 
 def submodules_generator():
     """Returns a generator for all submodule add-ons."""
     sub_path = "./subs"
-    # do_not_use = ["solver"]
-    do_not_use = []
+    do_not_use = ["solver.py", 'ping.py', 'pokemon.py', 'gastercoin.py']
     for item in os.listdir(sub_path):
         path = os.path.join(sub_path, item)
         if item not in do_not_use:
             for sub in os.listdir(path):
                 if sub == f"{item}.py" and sub not in do_not_use:
                     yield f"subs.{item}.{sub[:-3]}"
+    # use = []
+    # for item in os.listdir(sub_path):
+    #     path = os.path.join(sub_path, item)
+    #     if item in use:
+    #         for sub in os.listdir(path):
+    #             if sub == f"{item}.py" and sub in use:
+    #                 yield f"subs.{item}.{sub[:-3]}"
 
 DESCRIPTION = "A basic bot that runs a couple of uninteresting cogs."
 
-# logger = logging.getLogger(__name__)
+# log = logging.getLogger(__name__)
 
 class MathBot(commands.Bot):
     """Defines the mathbot class and functions."""
 
     def __init__(self):
-        super().__init__(command_prefix=["$", "!"], description=DESCRIPTION)
-        self.default_nick = "MathBot"
+        super().__init__(command_prefix=["~"], description=DESCRIPTION)
+        self.default_nick = "Miniscape"
+        self.add_command(self.load)
 
         for extension in extensions_generator():
             try:
@@ -52,10 +64,6 @@ class MathBot(commands.Bot):
                 print(f'Failed to load submodule {submodule}.', file=sys.stderr)
                 traceback.print_exc()
 
-    def run(self):
-        """Runs the bot with the token from the config file."""
-        super().run(config.token, reconnect=True)
-
     async def on_ready(self):
         """Prints bot initialization info"""
         print('Logged in as')
@@ -67,10 +75,24 @@ class MathBot(commands.Bot):
         """Handles commands based on messages sent"""
         if message.author.bot:
             return
-
         await self.process_commands(message)
+
+    def run(self):
+        """Runs the bot with the token from the config file."""
+        super().run(config.token, reconnect=True)
 
     async def on_member_update(self, before, after):
         """Resets bot's nickname anytime it is changed."""
         if before.id == self.user.id and before.nick != after.nick:
             await after.edit(nick=self.default_nick)
+
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, extension):
+        """Loads a specified extension into the bot."""
+        try:
+            self.load_extension(extension)
+            await ctx.send(f"Successfully loaded extension {extension}.")
+        except Exception:
+            await ctx.send(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
