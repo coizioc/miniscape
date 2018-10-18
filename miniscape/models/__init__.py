846e9b86eaff6292a2a30193b53d1c50d146bca5
Mitch Usher <cull.methi@gmail.com>
2018-10-18 19:27:42 -0400

    add inits

---
+++ miniscape/models/__init__.py
@@ -0,0 +1,10 @@
+from .user import User
+from .item import ItemNickname, Item
+from .quest import Quest, QuestItemRequirements, QuestItemRewards, UserQuest
+from .monster import Monster, MonsterLoot, MonsterNickname
+from .prayer import Prayer, PrayerNickname
+from .userinventory import UserInventory
+from .recipe import Recipe, RecipeRequirement
+from .playermonsterkills import PlayerMonsterKills
+from .clueloot import ClueLoot
+
