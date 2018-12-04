f15c7634f66a1e5bdd6aff293bcff42e59ca1dd6
coizioc <36041795+coizioc@users.noreply.github.com>
2018-12-04 16:03:01 -0500

    Adjusted drop rate for Christmas crackers.

--- miniscape/models/monster.py
+++ miniscape/models/monster.py
@@ -111,15 +111,15 @@ class Monster(models.Model):
                 logging.getLogger(__name__).info(log_str)
                 loot[ml.item] += amount
 
-        # 1% chance to drop christmas cracker per kill for Christmas event.
-        if random.randint(1, 10000) == 1:
-            christmas_cracker = Item.objects.get(name='christmas cracker')
-            log_str = "Awarded %d %s as part of loot generation for %d %s" % (1,
-                                                                              christmas_cracker.name,
-                                                                              int(num),
-                                                                              self.name)
-            logging.getLogger(__name__).info(log_str)
-            loot[christmas_cracker] += 1
+            # Chance to drop christmas cracker per kill for Christmas event.
+            if random.randint(1, 1000000) == 1:
+                christmas_cracker = Item.objects.get(name='christmas cracker')
+                log_str = "Awarded %d %s as part of loot generation for %d %s" % (1,
+                                                                                  christmas_cracker.name,
+                                                                                  int(num),
+                                                                                  self.name)
+                logging.getLogger(__name__).info(log_str)
+                loot[christmas_cracker] += 1
 
         return loot
 
